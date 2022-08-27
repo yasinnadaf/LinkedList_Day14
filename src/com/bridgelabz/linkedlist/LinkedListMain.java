63439db4c4ef6a6351b92f510425cbca40f6b794
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-27 23:10:54 +0530

    Modified: add method added for append elements

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -8,6 +8,15 @@ public class LinkedListMain {
         linkedList.push(56);
 
         linkedList.display();
+        System.out.println();
+
+        LinkedList<Integer> linkedList1 = new LinkedList<>();
+
+        linkedList1.add(56);
+        linkedList1.add(30);
+        linkedList1.add(70);
+
+        linkedList1.display();
 
     }
 }
