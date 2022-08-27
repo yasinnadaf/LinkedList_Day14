3c99069fc43e419af3890915854251e5b6ae9e45
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-08-27 22:39:26 +0530

    Modified: push method added for adding elements to node

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -4,10 +4,22 @@ public class LinkedList<T> {
     Node<T> head;
     Node<T> tail;
 
+    void push(T data){
+        Node<T> node = new Node<>(data);
+        if(head == null){
+            head = node;
+            tail = node;
+        }
+        else{
+            node.next = head;
+            head = node;
+        }
+    }
+
     void display(){
         Node<T> temp = head;
         while (temp != null){
-            System.out.println(temp.data);
+            System.out.print(temp.data+ " ");
             temp = temp.next;
         }
     }
